22806be97532ad939ee1f5632a10f1cc836b19d4
Ricky Nelson <rickyn@socketwiz.com>
2020-08-30 17:12:31 -0400

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/2012-08-23-caps-lock":["static\u002Fchunks\u002Fpages\u002F2012-08-23-caps-lock.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
